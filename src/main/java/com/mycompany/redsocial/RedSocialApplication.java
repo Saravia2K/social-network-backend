9c3c321addea8666533695c42adcd251d5cb7979
arrevillagamario <marrevillaga03@gmail.com>
2025-02-26 18:31:21 -0600

    My firste commit

---
+++ src/main/java/com/mycompany/redsocial/RedSocialApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedSocialApplication.class, args);
+    }
+
+}
